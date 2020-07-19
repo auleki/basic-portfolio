de134b29a006eb6d546f2b9fc5c7f8e6fb12209b
auleki <kodagiwa@gmail.com>
2020-07-19 23:24:05 +0100

    project page demo

--- js/main.js
+++ js/main.js
@@ -20,5 +20,20 @@ window.onload = function () {
 //     }
 // }, 25)
 
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave(data) {
+            return gsap.to(data.current.container, {
+                opacity: 0
+            });
+        },
+        enter(data) {
+            return gsap.from(data.next.contanier, {
+                opacity: 0
+            })
+        }
 
+    }]
+})
 
