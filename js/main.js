6dd8ad6d5629dddc6f8520f4979420cf62a64ae2
auleki <kodagiwa@gmail.com>
2020-08-14 18:28:43 +0100

    responsive bugs fixed

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ document.addEventListener("mousemove", parallax);
         function parallax(e) {
             this.querySelectorAll('.layer').forEach(layer => {
                 const speed = layer.getAttribute("data-speed");
-                const x = (window.innerWidth - e.pageX*speed)/25;
-                const y = (window.innerHeight - e.pageY*speed)/65;
+                const x = (window.innerWidth - e.pageX*speed)/85;
+                const y = (window.innerHeight - e.pageY*speed)/85;
                 layer.style.transform = `translateX(${x}px) translateY(${y}px)`
             })
         }
